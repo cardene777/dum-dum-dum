1dcf4f90dd0ce573dbabc12389c00512ceeaeb13
cardene <chotips08@gmail.com>
2024-09-12 01:21:23 +0900

    💫 redeploy

--- src/web/vite.config.ts
+++ src/web/vite.config.ts
@@ -5,40 +5,41 @@ import { nodePolyfills } from "vite-plugin-node-polyfills";
 import { resolve } from "path";
 import generouted from "@generouted/react-router/plugin";
 
-export default defineConfig({
-  plugins: [
-    nodePolyfills({
-      include: ["buffer", "crypto", "stream", "util", "http", "path"],
-    }),
-    react(),
-    generouted(),
-    tsconfigPaths(),
-  ],
-  // base: "/dum-dum-dum/",
-  // base: "/",
-  // build: {
-  //   outDir: "../../dist", // dist 出力をプロジェクトのルートに出力
-  //   emptyOutDir: true,
-  // },
-  // build: {
-  //   outDir: "dist", // デフォルトの dist ディレクトリを使用
-  //   emptyOutDir: true,
-  // },
-  root: "./",
-  build: {
-    // distフォルダに出力
-    outDir: resolve(__dirname, "dist"),
-    // 存在しないときはフォルダを作成する
-    emptyOutDir: true,
-    rollupOptions: {
-      // entry pointがあるindex.htmlのパス
-      input: {
-        "": resolve(__dirname, "index.html"),
-      },
-      // bundle.jsを差し替えする
-      output: {
-        entryFileNames: `assets/bundle.js`,
+export default defineConfig((configEnv) => {
+  const isDevelopment = configEnv.mode === "development";
+  return {
+    plugins: [
+      nodePolyfills({
+        include: ["buffer", "crypto", "stream", "util", "http", "path"],
+      }),
+      react(),
+      generouted(),
+      tsconfigPaths(),
+    ],
+    root: "./",
+    build: {
+      outDir: resolve(__dirname, "dist"),
+      emptyOutDir: true,
+      rollupOptions: {
+        input: {
+          "": resolve(__dirname, "index.html"),
+        },
+        output: {
+          entryFileNames: `assets/bundle.js`,
+        },
       },
     },
-  },
+    css: {
+      modules: {
+        generateScopedName: isDevelopment
+          ? "[name]__[local]__[hash:base64:5]"
+          : "[hash:base64:5]",
+      },
+    },
+    preprocessorOptions: {
+      css: {
+        charset: false,
+      },
+    },
+  };
 });
