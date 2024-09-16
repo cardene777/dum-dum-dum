45bbf8700e0f18fc597f4d71a8ecce755d8b576a
Tomoya Nagasawa <ocrybit@gmail.com>
2024-09-16 20:33:44 +0400

    use HashRouter

--- src/web/src/react-router/AppRoutes.tsx
+++ src/web/src/react-router/AppRoutes.tsx
@@ -1,33 +1,31 @@
-import { BrowserRouter, Route, Routes } from "react-router-dom";
-import App from "../App.tsx";
-import Illustration from "../pages/Illustration.tsx";
-import MyCard from "../pages/MyCard.tsx";
-import Collections from "../pages/Collections.tsx";
-import Ranking from "../pages/Ranking.tsx";
-import BattleUser from "../pages/BattleUser.tsx";
-import Shop from "../pages/Shop.tsx";
-import OpenPack from "../pages/OpenPack.tsx";
-import PackDetails from "../pages/PackDetails.tsx";
-import PackResult from "../pages/PackResult.tsx";
-import Nft from "../pages/Nft.tsx";
-const AppRoutes = () => {
-  return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<App />} />
-        <Route path="/illustration" element={<Illustration />} />
-        <Route path="/my-card" element={<MyCard />} />
-        <Route path="/collections" element={<Collections />} />
-        <Route path="/ranking" element={<Ranking />} />
-        <Route path="/battle-user" element={<BattleUser />} />
-        <Route path="/shop" element={<Shop />} />
-        <Route path="/open-pack" element={<OpenPack />} />
-        <Route path="/pack-details" element={<PackDetails />} />
-        <Route path="/pack-result" element={<PackResult />} />
-        <Route path="/nft" element={<Nft />} />
-      </Routes>
-    </BrowserRouter>
-  );
-};
+import { RouterProvider, createHashRouter } from "react-router-dom"
+import App from "../App.tsx"
+import Illustration from "../pages/Illustration.tsx"
+import MyCard from "../pages/MyCard.tsx"
+import Collections from "../pages/Collections.tsx"
+import Ranking from "../pages/Ranking.tsx"
+import BattleUser from "../pages/BattleUser.tsx"
+import Shop from "../pages/Shop.tsx"
+import OpenPack from "../pages/OpenPack.tsx"
+import PackDetails from "../pages/PackDetails.tsx"
+import PackResult from "../pages/PackResult.tsx"
+import Nft from "../pages/Nft.tsx"
 
-export default AppRoutes;
+const router = createHashRouter([
+  { path: "/", element: <App /> },
+  { path: "/illustration", element: <Illustration /> },
+  { path: "/my-card", element: <MyCard /> },
+  { path: "/collections", element: <Collections /> },
+  { path: "/ranking", element: <Ranking /> },
+  { path: "/battle-user", element: <BattleUser /> },
+  { path: "/shop", element: <Shop /> },
+  { path: "/open-pack", element: <OpenPack /> },
+  { path: "/pack-details", element: <PackDetails /> },
+  { path: "/pack-result", element: <PackResult /> },
+  { path: "/nft", element: <Nft /> },
+])
+const AppRoutes = () => {
+  return <RouterProvider router={router} />
+}
+
+export default AppRoutes
